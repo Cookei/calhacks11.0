a87647c829092fb283ac8fdba239d716c3a6979d
Olivia Xie <olivia.xie@berkeley.edu>
2024-10-20 08:41:42 -0700

    add listing container

--- what2eat/src/components/ListingContainer.js
+++ what2eat/src/components/ListingContainer.js
@@ -30,6 +30,15 @@ const ListingContainer = ({ datas }) => {
             distance: "5mi",
             review: 4.5,
             cuisine: "chinese, boba",
+        },
+        {
+            name: "Yi Fang",
+            icon: "https://s3-media0.fl.yelpcdn.com/bphoto/LGKisGL1yTe_5GnGwZ0o0g/348s.jpg",
+            price: 2,
+            location: "https://maps.app.goo.gl/pyR4gcozv1d125i49",
+            distance: "5mi",
+            review: 4.5,
+            cuisine: "chinese, boba",
         }
     ];
 
@@ -37,17 +46,20 @@ const ListingContainer = ({ datas }) => {
 
     for (let i = 0; i < datas.length; i++) {
         listings.push(
-            <Listing data={datas[i]}/>
+            <div className="listingContainerRow">
+                <h1 style={{width: "10px"}}>{i+1}</h1>
+                <Listing data={datas[i]}/>
+            </div>
+
         );
     }
 
     return (
-        <div className ="listing">
+        <div className ="listingContainer">
             {
                 listings
             }
         </div>
-
     );
 };
 
